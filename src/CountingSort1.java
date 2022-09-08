e2483f568afd1b0f8aac35c04e4c2ebf65af6f63
master <wjddn3711@naver.com>
2022-09-08 23:42:16 +0900

    Hackerrank 문제 풀이 - 0~ 100 까지 arr 안의 요소중 등장 횟수를 구하는 간단한 문제 - Collections.frequency 를 활용하면 Collection 안의 element 의 개수를 쉽게 구할 수 있다

---
+++ src/CountingSort1.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class ResultCountingSort1 {
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> countingSort(List<Integer> arr) {
+        // Write your code here
+        // simple question of 0~100 frequency of arr
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            answer.add(Collections.frequency(arr, i));
+        }
+
+        return answer;
+    }
+
+}
+
+public class CountingSort1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = ResultCountingSort1.countingSort(arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
