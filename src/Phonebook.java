0efd92624106b8d7f876a7ddf04bb1f86e937dcb
master <wjddn3711@naver.com>
2022-09-24 19:47:18 +0900

    프로그래머스 전화번호 목록 문제 풀이 - 문자열을 정렬한 이후, 에는 "119", "11955xxxxx" 이런식으로 정렬됨 - 해당하는 문자가 prefix 인지 여부확인법? startswith 메서드를 활용

---
+++ src/Phonebook.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Phonebook {
+
+
+    public static void main(String[] args) {
+        boolean answer = true;
+        String[] phone_book = {"123","456","789"};
+        // 하나의 번호가 다른 하나의 번호의 prefix 인 경우를 찾는다
+        Arrays.sort(phone_book);
+        // 비교해야할 지점은 i번째 번호의 전체가 0~length 만큼 일치하는지 확인하는것
+
+        for (int i = 0; i < phone_book.length-1; i++) {
+            if(phone_book[i+1].startsWith(phone_book[i])){
+                answer = false;
+                break;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
